60348707dae68f04f4cfe7a66725d2a6d4b75687
Tyler Long <tyler4long@gmail.com>
2021-09-13 21:40:48 -0700

    Show file list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
-const Home = () => <h1>hello world!</h1>;
+import React from 'react';
+import {fileNames} from '../src/utils';
+
+class Home extends React.Component<{fileNames: string[]}> {
+  render() {
+    const {fileNames} = this.props;
+    return (
+      <ul>
+        {fileNames.map(fn => (
+          <li key="fn">{fn}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default Home;
+
+export const getServerSideProps = () => {
+  return {
+    props: {
+      fileNames,
+    },
+  };
+};
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+import fs from 'fs';
+
+export const fileNames = fs.readdirSync('/Users/tyler.liu/Desktop');
